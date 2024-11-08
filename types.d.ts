78bce08621153bc2cd5475c7f6b516d5a998d57c
Sebastian Wiesner <sebastian@swsnr.de>
2024-11-08 10:12:38 +0100

    Use interface instead of type
    Fixes eslint error

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,11 @@
 declare module "eslint-plugin-promise" {
   import type { TSESLint } from "@typescript-eslint/utils";
 
-  type PluginPromise = {
-    configs: {
+  interface PluginPromise {
+    readonly configs: {
       "flat/recommended": TSESLint.FlatConfig.Config;
     };
-  };
+  }
 
   declare const pluginPromise: PluginPromise;
 
